831fb774a6b4f00ac601d79f07afc2370ff70cec
Igor Boyko <iboyko@gmail.com>
2022-05-24 17:33:07 -0700

    added .env file

--- server/app.js
+++ server/app.js
@@ -1,8 +1,36 @@
 const express = require('express');
-
+const axios = require('axios');
+const morgan = require('morgan');
+require('dotenv').config();
+// console.log(process.env); 
 
 const app = express();
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+// Server should log each request using morgan's dev format
+app.use(morgan('dev'));
+
+var requestCount = 0;
+app.use(function(req, res, next) {
+    requestCount++;
+    console.log('Request number: ' + requestCount);
+    next();
+});
+
+const api_key = process.env.API_KEY;
+const url = `http://www.omdbapi.com/?i=tt3896198&apikey=${api_key}`;
+
+
+// Server should respond to GET requests to /?i=tt3896198 with movie data
+app.get('/', function(req, res) {
+    // res.status(200).send('This is the / route'); 
+    axios.get(url)
+    // .then(response => res.json(response.data));
+    .then(function (response) {
+        res.json(response.data);
+
+        console.log("Response.data: " + response.data); 
+        console.log("Res: " + res); 
+    });
+});
 
 module.exports = app;
