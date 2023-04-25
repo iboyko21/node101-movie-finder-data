8445c8e2a7394373cc6677890eff2f0928a0b8cf
Igor Boyko <iboyko@gmail.com>
2023-04-25 09:15:07 -0700

    minor fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const server = require('./app');
 
-
 // Server should indicate when it is listening and on which port
 server.listen(3000, function() {
   console.log('Server is listening on http://localhost:3000');
